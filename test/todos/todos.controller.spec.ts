336a47d2784fa33fff1002e80d802b8c57bb2847
PandaHawk <mchlbng@gmail.com>
2025-02-14 10:58:19 +0100

    add first test

--- test/todos/todos.controller.spec.ts
+++ test/todos/todos.controller.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosController } from '../../src/todos/todos.controller';
 import { TodosService } from '../../src/todos/todos.service';
+import { randomUUID } from 'crypto';
 
 jest.mock('../../src/todos/todos.service');
 
 describe('TodosController', () => {
   let controller: TodosController;
-  let service: TodosService;
+  let mockService: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,11 +16,27 @@ describe('TodosController', () => {
     }).compile();
 
     controller = module.get<TodosController>(TodosController);
-    service = module.get<TodosService>(TodosService);
+    mockService = module.get<TodosService>(TodosService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-    expect(service).toBeDefined();
+    expect(mockService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a array of todos', async () => {
+      const mockTodos = [
+        { id: randomUUID(), task: 'task 1', completed: false },
+        { id: randomUUID(), task: 'task 2', completed: false },
+      ];
+
+      (mockService.findAll as jest.Mock).mockReturnValue(mockTodos);
+
+      const result = await controller.findAll();
+
+      expect(mockService.findAll).toHaveBeenCalled();
+      expect(result).toEqual(mockTodos);
+    });
   });
 });
