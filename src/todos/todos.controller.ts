dc44d2153564d9cebf654c7c3affc40f87b54c55
PandaHawk <mchlbng@gmail.com>
2025-02-14 10:58:19 +0100

    add first test

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { Todo } from './interfaces/todo.interface';
 
 @Controller('todos')
 export class TodosController {
@@ -9,8 +10,8 @@ export class TodosController {
     this.todoService = todoService;
   }
 
-  /*   @Get()
+  @Get()
   async findAll(): Promise<Todo[]> {
-    return await this.todoService.findAll();
-  } */
+    return this.todoService.findAll();
+  }
 }
